9a5b77fccc8a9710b8567a737f105b2540a9dc0f
d0mitorii <domitoriiforwork@gmail.com>
2024-04-18 23:21:34 +0300

    fix: send form button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,7 @@ function HomepageHeader() {
           <div className={styles.buttons}>
             <Link
               className="button button--primary button--lg"
-              to="/about"
+              to="https://docs.google.com/forms/d/e/1FAIpQLSd439lToKD3iWyeyhokr8FDmnviJtvespjAiwM_0_tl2tLbHw/viewform"
             >
               Подать заявку
             </Link>
