15033655cf717e61b93a5ce42745db0139c6547b
d0mitorii <domitoriiforwork@gmail.com>
2024-04-20 22:42:43 +0300

    create cardComponent

--- docs/guidelines/mechanics/index.md
+++ docs/guidelines/mechanics/index.md
@@ -7,5 +7,13 @@ slug: /mechanics
 # Механики
 
 import DocCardList from '@theme/DocCardList';
+import { CardSection, Card } from "@site/src/components/CardComponent"
 
-<DocCardList />
+<CardSection>
+  <Card 
+    title="Зип-рельсы"
+    description="Описание и руководство по использованию зип-рельсов для путешествий на вагонетке."
+    preview="/img/mechanics/ziprails-banner.png"
+    to="/docs/mechanics/ziprails"
+  />
+</CardSection>
---
+++ src/components/CardComponent.tsx
@@ -0,0 +1,32 @@
+import Link from '@docusaurus/Link';
+import React from 'react';
+
+export function CardSection(props) {
+  return (
+    <section className='row'>
+      {props.children}
+    </section>
+  );
+}
+
+export function Card(props) {
+  return (
+    <article className='col col--6 margin-bottom--lg'>
+      <Link
+        to={props.to}
+        className="card padding--lg cardContainer_node_modules-@docusaurus-theme-classic-lib-theme-DocCard-styles-module"
+        style={{height: "100%"}}
+      >
+        <div className=''>
+          <div className='img-card margin-bottom--md'>
+            <img src={props.preview} className='' style={{flex: 1, height: "100%"}}/>
+          </div>
+          <h3>
+            {props.title} {props.new ? <span className="badge badge--info">Новинка</span> : null}
+          </h3>
+          <p className='hardshard-p'>{props.description}</p>
+        </div> 
+      </Link>
+    </article>
+  );
+}
