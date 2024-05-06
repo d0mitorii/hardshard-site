a9adb41e20521c30d2439f9801fd17253e314843
d0mitorii <domitoriiforwork@gmail.com>
2024-05-07 01:56:23 +0300

    feat: PlayersListComponents

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,63 +2,10 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-// import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
-import Heading from '@theme/Heading';
-import { useLayoutEffect, useState } from 'react';
-import { PhotoNamePlayer } from '../components/PhotoNamePlayer';
-
-//todo: loader
-const fetchPlayerList = async () => {
-  try {
-    const response = await fetch("https://api.mcsrvstat.us/3/mc.hardshard.ru");
-    if (!response.ok) {
-      throw new Error("Error in request or response.");
-    }
-    const data = await response.json();
-    return [data.players.online, data.players.max, data.players.list] || []; // Возвращаем массив игроков или пустой массив, если данные не найдены
-  } catch (error) {
-    console.error("Error:", error);
-    return []; // Возвращаем пустой массив в случае ошибки
-  }
-};
-
-const PlayersList = () => {
-  const [playersList, setPlayersList] = useState([]);
-  const [online, setOnline] = useState<number>(0);
-  const [maxPlayers, setMaxPlayers] = useState<number>(0);
-
-  useLayoutEffect(() => {
-    const fetchData = async () => {
-      const playerList = await fetchPlayerList();
-      setOnline(playerList[0]);
-      setMaxPlayers(playerList[1]);
-      setPlayersList(playerList[2]);
-    };
-    fetchData();
-  }, []);
-  return (
-    <div className="row margin-top--lg margin-bottom--lg">
-      <div 
-        className="col col--8 col--offset-2 text--center padding--lg "
-        style={{
-          backgroundColor: "var(--ifm-card-background-color)",
-          borderRadius: "var(--ifm-card-border-radius)",
-        }}
-      >
-        <h2>Игроков на сервере {online} / {maxPlayers}</h2>
-        <div style={{alignContent: "center"}}>
-          {
-            playersList.map((player, index) => (
-              <PhotoNamePlayer nickname={player.name} key={index}/>
-            ))
-          }
-        </div>
-      </div>
-    </div>
-  );
-};
+import { PlayersList } from '../components/PlayersListComponent';
 
 function HomepageHeader() {
 const {siteConfig} = useDocusaurusContext();
@@ -100,14 +47,14 @@ const {siteConfig} = useDocusaurusContext();
 export default function Home(): JSX.Element {
   return (
     <Layout
-    title="Приватный Vanila+ сервер"
-    description="Ламповый Vanilla+ бесплатный сервер без приватов, с улучшенным геймплеем и элементами ролевой игры.">
+      title="Приватный Vanila+ сервер"
+      description="Ламповый Vanilla+ бесплатный сервер без приватов, с улучшенным геймплеем и элементами ролевой игры."
+    >
       <HomepageHeader />
       <main>
         <div className="container">
           <PlayersList />
         </div>
-        {/* <HomepageFeatures /> */}
       </main>
     </Layout>
   );
