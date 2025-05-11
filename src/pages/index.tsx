a095f2388cd67187130c5e2b78565190223b5faf
Flammablelion <Flammablelion@yandex.ru>
2025-05-12 02:32:34 +0300

    feat: add backgound video in main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,22 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import Heading from '@theme/Heading';
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import Heading from "@theme/Heading";
 
-import styles from './index.module.css';
-import { PlayersList } from '../components/PlayersListComponent';
-import { SplashComponent } from '../components/SplashComponent';
-import { CopyButton } from '../components/CopyButton';
+import styles from "./index.module.css";
+import { PlayersList } from "../components/PlayersListComponent";
+import { SplashComponent } from "../components/SplashComponent";
+import { CopyButton } from "../components/CopyButton";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero padding-bottom--xl', styles.heroBanner)}>
+    <header className={clsx("hero padding-bottom--xl", styles.heroBanner)}>
+      <video autoPlay muted loop playsInline className={styles.backgroundVideo}>
+        <source src="/videos/background.webm" type="video/webm" />
+        Your browser does not support the video tag.
+      </video>
       <div className="container">
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
@@ -22,9 +26,7 @@ function HomepageHeader() {
             <p className="hero__subtitle">{siteConfig.tagline}</p>
           </div>
         </div>
-        <div className={"row"}
-          style={{ gap: 24, justifyContent: "center" }}
-        >
+        <div className={"row"} style={{ gap: 24, justifyContent: "center" }}>
           <div className={styles.buttons}>
             <Link
               className="button button--primary button--lg"
