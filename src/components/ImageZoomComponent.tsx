ba4018299dd718cf76207e95adeaa1d61a306899
Flammablelion <Flammablelion@yandex.ru>
2026-02-16 22:58:19 +0300

    fix: макс высота
    добавил возможность указывать максимальную высоту изображения в пропсы

--- src/components/ImageZoomComponent.tsx
+++ src/components/ImageZoomComponent.tsx
@@ -1,17 +1,26 @@
 export function ImageZoom(props) {
   return (
     <>
-      <div 
-        className="imageContainer" 
+      <div
+        className="imageContainer"
         style={{
           display: "flex",
           justifyContent: "center",
-          marginBottom: (props.marginBottom || props.description) ? props.marginBottom : "1.25rem",
+          marginBottom:
+            props.marginBottom || props.description
+              ? props.marginBottom
+              : "1.25rem",
         }}
       >
-        <img src={props.src} alt={props.alt}/>
+        <img
+          src={props.src}
+          alt={props.alt}
+          style={{ maxHeight: props.maxHeight || "100%" }}
+        />
       </div>
-        {props.description && <p className="text--center text--italic" >{props.description}</p>}
+      {props.description && (
+        <p className="text--center text--italic">{props.description}</p>
+      )}
     </>
   );
 }
